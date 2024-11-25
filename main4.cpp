755be4578e6af6b2754a007480ee097528ad1e23
GMtimur <gmtimurtwitch@gmail.com>
2024-11-25 23:18:52 +0300

    Main

--- main4.cpp
+++ main4.cpp
@@ -1,24 +1,19 @@
 #include <iostream>
+#include <cstring> 
 using namespace std;
 
-
-const int COLS = 10;
-const int ROWS = 10;
-
-void MulMat(int Mat1[ROWS][COLS], int Mat2[ROWS][COLS], int result[ROWS][COLS], int N, int M, int K) {
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < K; j++)
-        {
+void MulMat(int** Mat1, int** Mat2, int** result, int N, int M, int K) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < K; j++) {
             result[i][j] = 0;
-            for (int k = 0; k < M; k++)
-            {
+            for (int k = 0; k < M; k++) {
                 result[i][j] += Mat1[i][k] * Mat2[k][j];
             }
         }
     }
 }
-void powMat(int Mat[ROWS][COLS], int result[ROWS][COLS], int x, int N) {
+
+void powMat(int** Mat, int** result, int x, int N) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             if (i == j) {
@@ -31,15 +26,22 @@ void powMat(int Mat[ROWS][COLS], int result[ROWS][COLS], int x, int N) {
     }
 
     for (int p = 0; p < x; p++) {
-        int temp[ROWS][COLS] = { 0 };
+        int** temp = new int* [N];
+        for (int i = 0; i < N; i++)
+            temp[i] = new int[N]();
+
         MulMat(result, Mat, temp, N, N, N);
         for (int i = 0; i < N; i++)
             for (int j = 0; j < N; j++)
                 result[i][j] = temp[i][j];
+
+        for (int i = 0; i < N; i++)
+            delete[] temp[i];
+        delete[] temp;
     }
 }
 
-void prtMat(int matrix[ROWS][COLS], int N, int M) {
+void prtMat(int** matrix, int N, int M) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
             cout << matrix[i][j];
@@ -51,13 +53,9 @@ void prtMat(int matrix[ROWS][COLS], int N, int M) {
     }
 }
 
-
-
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
     bool isHuman = false;
-    if (argc <= 1 || strcmp(argv[1], "false") != 0)
-    {
+    if (argc <= 1 || strcmp(argv[1], "false") != 0) {
         isHuman = true;
     }
 
@@ -67,7 +65,10 @@ int main(int argc, char* argv[])
     if (isHuman) cout << "введите кол-во строк и столбцов Матрицы 1:" << endl;
     cin >> N >> M;
 
-    int A1[COLS][ROWS];
+    int** A1 = new int* [N];
+    for (int i = 0; i < N; i++)
+        A1[i] = new int[M];
+
     if (isHuman) cout << "введите элементы матрицы построчно:" << endl;
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
@@ -92,7 +93,10 @@ int main(int argc, char* argv[])
             if (isHuman) cout << "введите кол-во столбцов матрицы А2" << endl;
             cin >> K;
 
-            int A2[ROWS][COLS];
+            int** A2 = new int* [M];
+            for (int i = 0; i < M; i++)
+                A2[i] = new int[K];
+
             if (isHuman) cout << "введите элементы матрицы А2:" << endl;
             for (int i = 0; i < M; i++) {
                 for (int j = 0; j < K; j++) {
@@ -105,18 +109,30 @@ int main(int argc, char* argv[])
                 continue;
             }
 
-            int result[ROWS][COLS];
+
+            int** result = new int* [N];
+            for (int i = 0; i < N; i++)
+                result[i] = new int[K];
+
             MulMat(A1, A2, result, N, M, K);
 
 
             M = K;
-
-
             for (int i = 0; i < N; i++)
                 for (int j = 0; j < K; j++)
                     A1[i][j] = result[i][j];
-        }
 
+
+            for (int i = 0; i < N; i++)
+                delete[] result[i];
+            delete[] result;
+
+
+            for (int i = 0; i < M; i++)
+                delete[] A2[i];
+            delete[] A2;
+
+        }
         else if (command == 3) {
             if (N != M) {
                 cout << "NO" << endl;
@@ -125,15 +141,31 @@ int main(int argc, char* argv[])
             int x;
             if (isHuman) cout << "степень: " << endl;
             cin >> x;
-            int result[10][10];
+
+            int** result = new int* [N];
+            for (int i = 0; i < N; i++)
+                result[i] = new int[N];
+
             powMat(A1, result, x, N);
+
+
             for (int i = 0; i < N; i++)
                 for (int j = 0; j < N; j++)
                     A1[i][j] = result[i][j];
-        }
-        else {
-            if (isHuman){ cout << "ошибка " << endl;}
-            else{ cout << "NO" << endl; }
 
+            for (int i = 0; i < N; i++)
+                delete[] result[i];
+            delete[] result;}
+        else {
+            if (isHuman) { cout << "ошибка " << endl; }
+            else { cout << "NO" << endl; }
         }
-    }}
+    }
+
+
+    for (int i = 0; i < N; i++)
+        delete[] A1[i];
+    delete[] A1;
+
+    return 0;
+}
